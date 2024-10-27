125ef9a3fcad1983f21f19aa3f7ca909acc61521
pontakornth <pontakorn61@gmail.com>
2024-10-27 17:55:24 +0700

    refactor: Prettier format

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { Elysia } from "elysia";
+import { db } from "./db/database";
+import { taskTable } from "./db/schema";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
-
+const app = new Elysia()
+	.get("/", () => "Hello Elysia")
+	.listen(3000)
+	.group("/tasks", (app) => {
+		return app.get("/", () => {
+			const tasks = db.select().from(taskTable);
+			return tasks;
+		});
+	});
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
